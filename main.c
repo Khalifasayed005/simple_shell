4dd20d7bbe6bcb7a270cbba0673e5c70d38ff288
Mortaja2000 <139490377+Mortaja2000@users.noreply.github.com>
2023-10-15 03:59:07 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - entry point
- * @ac: arg count
+ * @ag: arg count
  * @av: arg vector
  * Return: 0 on success, 1 on error
 */
-int main(int ac, char **av)
+int main(int ag, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (ac == 2)
+	if (ag == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
